1a3b19969184c5b6cae409516d4117e579d1385a
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-01-24 11:06:48 -0300

    Adjust supabase variables

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  'https://edlcictxpnwiriizcylf.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbGNpY3R4cG53aXJpaXpjeWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3MjM4MjksImV4cCI6MjA1MzI5OTgyOX0.7NAGGPrAKT8wYU67wG5npHedBrPBfds3m7ncV69GIFQ'
 );
