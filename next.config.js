25b07844b0e359f26b5698b4599c7ff6585fe999
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-01-25 09:00:36 -0300

    More adjustments to config and workflow

--- next.config.js
+++ next.config.js
@@ -13,16 +13,6 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
-      },
     ],
   },
 };
