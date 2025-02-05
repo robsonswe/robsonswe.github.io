f2dde8ec22bf6bca648c27efff29e12828af3ec8
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-02-05 12:45:20 -0300

    Fix next image

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next"
-import Image from "next/image"
+import Image from "next/legacy/image"
 import Link from "next/link"
 import Icon from "./svgicons"
 import { useProjects } from "../hooks/useProjects"
