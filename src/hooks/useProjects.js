63465a49b197b9767c964e3ec74180266a671aa0
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-02-06 10:31:56 -0300

    Modify project and skills hooks

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,18 +1,7 @@
-import { useState, useEffect } from 'react';
 import fireProjects from '../data/projects.json';
 
-export const useProjects = () => {
-  const [projectsData, setProjectsData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      setProjectsData(fireProjects);
-      setLoading(false);
-    };
-
-    fetchProjects();
-  }, []);
-
-  return { projectsData, loading, error: null };
-};
+export const useProjects = () => ({
+  projectsData: fireProjects,
+  loading: false,
+  error: null,
+});
--- src/hooks/useSkills.js
+++ src/hooks/useSkills.js
@@ -1,18 +1,7 @@
-import { useState, useEffect } from 'react';
 import fireSkills from '../data/skills.json';
 
-export const useSkills = () => {
-  const [skillsData, setSkillsData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchSkills = async () => {
-      setSkillsData(fireSkills);
-      setLoading(false);
-    };
-
-    fetchSkills();
-  }, []);
-
-  return { skillsData, loading, error: null };
-};
+export const useSkills = () => ({
+  skillsData: fireSkills,
+  loading: false,
+  error: null,
+});
